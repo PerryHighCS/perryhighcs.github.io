fef0438c8d33df5ede82f2010038f5ab8d36eb33
mrbdahlem <dahlem.brian@cusd80.com>
2017-08-27 19:13:29 -0700

    Update notebook.js
    comments

--- notebook.js
+++ notebook.js
@@ -1,44 +1,91 @@
-function println(text) {
+// Display text in a textArea console
+function print(text) {
+  // Get a reference to the console
   var con = document.getElementById('Console');
-  con.style.display = "block";
+
+  // Get the current console text
   var conTxt = con.textContent;
-  conTxt += text + '\n';
+
+  // Append the new text
+  conTxt += text;
+
+  // Set the console text
   con.textContent = conTxt;
+
+  // Make sure the console is visible
+  con.style.display = "block";
 }
 
+// Display text in a textArea console, followed by a newline
+function println(text) {
+  print(text + '\n');
+}
+
+// Read a line of user input
 function readLine(promptTxt) {
-  return prompt(promptTxt)
+  // Prompt the user for input
+  rval = prompt(promptTxt);
+
+  // If the user clicks Cancel, throw an exception
+  if (rval === null) {
+    throw 'Exception: User Cancelled Input';
+  }
+
+  // Otherwise, return the input
+  return rval;
 }
 
+// Ask the user for a number
 function readInt(promptTxt) {
+  // Store the user prompt
   var text = promptTxt;
+
+  // Loop until we get our input
   while (true) {
+    // Ask the user for the number
     var rval = prompt(text);
+
+    // If the user clicks cancel, throw an exception
     if (rval === null) {
       throw 'Exception: User Cancelled Input';
     }
 
+    // Parse out the number the user entered
     rval = parseInt(rval);
+
+    // If the user didn't enter a number, ask again
     if (isNaN(rval)) {
       text = promptTxt + "\nPlease enter an integer.";
     } else {
+      // If they did enter a number, return it
       return rval;
     }
   }
 }
 
+// Ask the user for a number
 function readFloat(promptTxt) {
+  // Store the user prompt
   var text = promptTxt;
+
+  // Loop until we get our input
   while (true) {
+    // Ask the user for the number
     var rval = prompt(text);
+
+    // If the user clicks cancel, throw an exception
     if (rval === null) {
       throw 'Exception: User Cancelled Input';
     }
 
+    // Parse out the number the user entered
     rval = parseFloat(rval);
+
+    // If the user didn't enter a number, ask again
     if (isNaN(rval)) {
       text = promptTxt + "\nPlease enter a number.";
     } else {
+      // If they did enter a number, return it
       return rval;
     }
   }
