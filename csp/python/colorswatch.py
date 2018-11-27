39a70134004745bdefab7460d1b6d4143c2016dc
dahlem.brian <dahlem.brian@28T-F407-207718.cusd80.lz>
2018-11-27 12:42:25 -0700

    added colorswatch with a button python example

---
+++ csp/python/colorswatch.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import colorchooser
+
+# Define the event handler
+def ask_color(): # the event parameter is required
+    rgb_color, web_color = colorchooser.askcolor(parent=win,
+                                             initialcolor=(win['background']))
+    win['background']=web_color
+    colorLbl['text']=web_color
+
+win = tk.Tk()
+win.title('Swatch')
+win.geometry("500x500")
+
+# Create the user interface
+colorBtn = tk.Button(win, text="Click to choose a color.")
+colorBtn.grid(row=1, column=1)
+colorBtn['command'] = ask_color
+
+colorLbl = tk.Label(win, text="#")
+colorLbl.grid(row=1, column=2)
+
+# Start the event loop
+win.mainloop()
