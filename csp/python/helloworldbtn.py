1384207789cf9be1d091a147cae4f2db81fa5b96
dahlem.brian <dahlem.brian@28T-F407-207718.cusd80.lz>
2018-11-27 10:25:32 -0700

    added helloworld with a button python example

--- csp/python/helloworldbtn.py
+++ csp/python/helloworldbtn.py
@@ -8,7 +8,7 @@ my_label = tk.Label(win, text="Hello World!")
 my_label.grid(row=1, column=1)
 quit_button = tk.Button(win, text="Quit")
 quit_button.grid(row=2, column=1)
-quit_button['command'] = win.destroy
+quit_button['command'] = win.destroy  # no ()!
 
 # Start the GUI event loop
 win.mainloop()
